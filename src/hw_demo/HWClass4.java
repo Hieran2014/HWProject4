4859fcebe97e58cc8a2fb6dc3b5596019df197e6
GYTumlisan <gymoa2011@gmail.com>
2023-04-05 21:46:01 -0700

    Initial commit repository4

---
+++ src/hw_demo/HWClass4.java
@@ -0,0 +1,11 @@
+package hw_demo;
+
+public class HWClass4 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Local repository 4....");
+		
+		System.out.println("Remote repository 4....");
+	}
+}
